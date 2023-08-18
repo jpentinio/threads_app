f48bb8f4e001dc9f28ecb962f5cdb95825e3bc2c
jpentinio <joshuapentinio98@gmail.com>
2023-08-18 13:59:16 +0800

    fix error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
-import ErrorBoundary from "../../components/ErrorBoundary";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const inter = Inter({ subsets: ["latin"] });
 
